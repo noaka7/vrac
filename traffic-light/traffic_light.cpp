fbc935b939b2e02f4822cc39ff88826c2f4a6689
noaka7 <kphourat@gmail.com>
2021-05-22 04:07:04 +0200

    Add traffic-light/test.cpp

--- traffic-light/traffic_light.cpp
+++ traffic-light/traffic_light.cpp
@@ -19,9 +19,9 @@ TrafficLight::TrafficLight(COUNTRY country, COLOUR_STATE state) {
     std::cerr << "Error message: " << e.what() << std::endl;
   }
 }
-inline COUNTRY TrafficLight::get_country() const { return this->country; }
-inline COLOUR_STATE TrafficLight::get_state() const { return this->state; }
-inline int TrafficLight::count() const { return this->states.size(); }
+COUNTRY TrafficLight::get_country() const { return this->country; }
+COLOUR_STATE TrafficLight::get_state() const { return this->state; }
+int TrafficLight::count() const { return this->states.size(); }
 void TrafficLight::set_state(COLOUR_STATE state) {
   if (this->states.cend() == this->states.find(state)) {
     throw InvalidStateException();
@@ -115,40 +115,4 @@ std::ostream &operator<<(std::ostream &out, const COLOUR_STATE &colour) {
     break;
   }
   return out << s;
-}
-
-void test_traffic_light_set_state() {
-  TrafficLight light(FRA);
-  COLOUR_STATE c;
-  std::cout << "Test set state. Country: " << light.get_country() << std::endl;
-  std::cout << "State: " << light.get_state() << std::endl;
-  try {
-    c = BLUE;
-    std::cout << "Try to set invalid state: " << c << std::endl;
-    light.set_state(c);
-  } catch (const InvalidStateException &e) {
-    std::cerr << "Error message: " << e.what() << std::endl;
-  }
-  std::cout << "State: " << light.get_state() << std::endl;
-  try {
-    c = GREEN;
-    std::cout << "Try to set valid state: " << c << std::endl;
-    light.set_state(c);
-  } catch (const InvalidStateException &e) {
-    std::cerr << "Error message: " << e.what() << std::endl;
-  }
-  std::cout << "State: " << light.get_state() << std::endl;
-}
-
-int main() {
-  for (int i = 0; i < NB_OF_COUNTRY; i++) {
-    TrafficLight traffic_light = TrafficLight(static_cast<COUNTRY>(i));
-    std::cout << "country " << traffic_light.get_country() << std::endl;
-    for (int j = 0; j <= traffic_light.count(); j++) {
-      std::cout << traffic_light.get_state() << std::endl;
-      traffic_light.next();
-    }
-  }
-  test_traffic_light_set_state();
-  return 0;
 }
